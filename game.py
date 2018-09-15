2307ad05dba9939c4e8508a1b4fd123684fcea40
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-15 15:37:08 -0700

    fianl

--- game.py
+++ game.py
@@ -12,33 +12,42 @@ def play_game():
 
 
     while True:
+        guess = (input("Guess a number between 1 and 100 please "))
+
         try:
-            guess = int(input("Guess a number between 1 and 100 please "))
-
-            while True:
-                if guess > 100 or guess <1:
-                    print ("learn to read!!!")
-                    guess = int(input("Guess a correct number this time "))
+            guess = int(guess)
+            if guess > 100 or guess <1:
+                print ("learn to read!!!")
+                #guess = int(input("Guess a correct number this time "))
 
 
-                elif guess != random_number:
+            elif guess != random_number:
 
-                    if guess > random_number:
-                        print("Guess too high")
+                if guess > random_number:
+                    print("Guess too high")
 
-                    elif guess < random_number:
-                        print("Guess too low")
+                elif guess < random_number:
+                    print("Guess too low")
 
-                    guess = int(input("Guess again please "))
-                    num_guess += 1
+               # guess = int(input("Guess again please "))
+                num_guess += 1
 
-                else:
-                    print("Congrats! You guess correctly in {} tries".format(num_guess))
+            else:
+                if num_guess == 1:
+                    print("Congrats! You guessed correctly in {} try".format(num_guess))
                     break
-
+                else:
+                    print("Congrats! You guessed correctly in {} tries".format(num_guess))
+                    break
 
         except ValueError:
             print("Guess a number please ")
+
+
+
+
+
+
 
 
 
